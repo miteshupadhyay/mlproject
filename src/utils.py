7e1bf1e293acd2c948bb67c0109552d4437ca8a5
DevanshiBhatt <bhatt.devanshi202@gmail.com>
2024-06-16 13:41:50 +0530

    Full Code

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -20,16 +20,23 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models):
     try:
         report = {}
 
         for i in range(len(list(models))):
-            model = list (models.values())[i]
+            model = list(models.values())[i]
+            #para=param[list(models.keys())[i]]
 
-            model.fit(X_train,y_train)  # Train Model
+            #gs = GridSearchCV(model,para,cv=3)
+            #gs.fit(X_train,y_train)
 
-            y_train_pred =  model.predict(X_train)
+            #model.set_params(**gs.best_params_)
+            #model.fit(X_train,y_train)
+
+            model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
 
@@ -38,6 +45,16 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
+
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
