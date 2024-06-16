b21a9952cf1b1ac063444b07e57b9caeb93c3b8a
DevanshiBhatt <bhatt.devanshi202@gmail.com>
2024-06-16 13:51:10 +0530

    EBS Deployment

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -7,9 +7,17 @@ class PredictPipeline:
     def __init__(self):
         pass
     def predict(self,features):
-        model_path = 'artifacts/model.pkl'
-        preprocessor_path='artifacts/preprocessor.pkl'
-        model = load_object(file_path=model_path)
+        try:
+            model_path = 'artifacts/model.pkl'
+            preprocessor_path='artifacts/preprocessor.pkl'
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            data_scaled =preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        except Exception as e:
+            raise CustomException(e,sys)
+
 
 class CustomData:
     def __init__(self,
@@ -35,7 +43,7 @@ class CustomData:
 
         self.writing_score = writing_score
 
-    def get_data_as_data_frame():
+    def get_data_as_data_frame(self):
         try:
             custom_data_input_dict = {
                 "gender": [self.gender],
